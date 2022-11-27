6f2296add2a861d32a9c5afc2c76b244ff7f197e
adalessa <dalessandro.ariel@gmail.com>
2022-11-27 10:34:06 +0100

    feat: add nix flake file to be able to install it with nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Tshort flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+    system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
+      pname = "tshort";
+      version = "v0.1.3";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "adalessa";
+        repo = pname;
+        rev = version;
+        sha256 = "gndbCJNGtL/5Zl/FHNR3tIlbHZpPm+suQKLXCLIgc4A=";
+      };
+
+      cargoSha256 = "0lqruGAIYh70O+ecQM8ubcnuzRFDi0+1ywXST3l+uVg=";
+
+      meta = with pkgs.lib; {
+        description = "A cli tool to manage tmux session";
+        homepage = "https://github.com/adalessa/tshort";
+        license = licenses.unlicense;
+        maintainers = [ maintainers.tailhook ];
+      };
+    };
+  };
+}
